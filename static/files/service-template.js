74e80cd018ad2e18c6292d25f5ced5c03bc5320d
Nidhi Rastogi <nidhi.gupta@gmail.com>
2023-09-06 15:05:33 -0400

    Removed all files

--- static/files/service-template.js
+++
@@ -1,8 +0,0 @@
-window.template = Handlebars.compile(`
-<div class="articles">
-    <ul>
-        {{#each .}}
-            <li><span class="type">{{Type}}</span> - {{{Content}}} </li>
-        {{/each}}
-    </ul>
-  </div>`);
